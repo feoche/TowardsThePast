0b5019568fc44b9aedf935d2c872f2583d172491
François Eoche <feoche@users.noreply.github.com>
2016-12-08 15:49:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(12345);
+var express         = require('express');
+var app             = express();								// Useful just for routing the files
+
+// Change accordingly to your app folder.
+var staticdir = process.env.NODE_ENV === 'production' ? 'dist.prod' : 'dist.dev';
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/' + staticdir));
+
+var http = require('http');
+
+// Entry point of the app
+app.get("/*", function(req, res) {
+    res.sendFile(__dirname + '/' + staticdir + "/index.html");
+});
+
+// Create an HTTP service.
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
+});
